bfcadd8e714df9383636aa0e1bea2dd92a4cab63
Laxmi <laxmipriyavanams@gmail.com>
2026-06-15 13:41:43 +0530

    Fix Railway port

--- server/index.js
+++ server/index.js
@@ -366,6 +366,8 @@ app.delete(
     }
   }
 );
-server.listen(3001, () => {
-  console.log("Server running on port 3001");
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
