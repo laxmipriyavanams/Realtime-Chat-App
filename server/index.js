c611ca9e67cc32910e16cf19597b2d917c44d47b
Laxmi <laxmipriyavanams@gmail.com>
2026-06-20 12:40:25 +0530

    Fix Railway host binding

--- server/index.js
+++ server/index.js
@@ -368,6 +368,6 @@ app.delete(
 );
 const PORT = process.env.PORT || 3001;
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
